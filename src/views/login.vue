e64f7afabe5d7e6c033e089026ad3fba8ff2df6b
chongchongjiwei <2284810742@qq.com>
2023-11-24 09:34:21 +0800

    first

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,21 +1,40 @@
 <template>
-    <div class="login-container">
-      <form @submit.prevent="submit">
-        <div>
-          <label for="username">账号：</label>
-          <input type="text" id="username" v-model="credentials.username" required>
-        </div>
-        <div>
-          <label for="password">密码：</label>
-          <input type="password" id="password" v-model="credentials.password" required>
-        </div>
-        <div>
-          <button type="submit">登录</button>
-        </div>
-      </form>
-    </div>
-  </template>
-  
-  <script lang="ts" src="./useLoginForm.ts"></script>
-  <!-- <style src="./loginFormStyles.css" scoped></style> -->
-  
+  <div class="login-container">
+    <form @submit.prevent="submitForm">
+      <div>
+        <label for="username">账号：</label>
+        <input type="text" id="username" v-model="credentials.username" required>
+      </div>
+      <div>
+        <label for="password">密码：</label>
+        <input type="password" id="password" v-model="credentials.password" required>
+      </div>
+      <div>
+        <button type="submit">登录</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { useLoginForm } from './useLoginForm.ts';
+
+export default defineComponent({
+  name: 'Login',
+  setup() {
+    const { credentials, submitForm } = useLoginForm();
+    return {
+      credentials,
+      submitForm
+    };
+  }
+});
+</script>
+
+<style>
+/* 这里可以添加CSS样式 */
+.login-container {
+  /* 样式定义 */
+}
+</style>
--- src/views/useLoginForm.ts
+++ src/views/useLoginForm.ts
@@ -1,22 +1,21 @@
 import { reactive, toRefs } from 'vue';
 
-export default {
-  name: 'LoginForm',
-  setup() {
-    const credentials = reactive({
+export function useLoginForm() {
+  const state = reactive({
+    credentials: {
       username: '',
-      password: '',
-    });
-
-    function submit() {
-      // 这里应该有调用后端API进行登录验证的代码
-      console.log('Submitted:', credentials);
+      password: ''
     }
+  });
 
-    // 将响应式对象转换为引用以在模板中使用
-    return {
-      ...toRefs(credentials),
-      submit,
-    };
-  },
-};
+  const submitForm = () => {
+    // 这里可以添加调用后端API进行登录验证的代码
+    console.log('Form Submitted:', state.credentials);
+    // 处理登录逻辑
+  };
+
+  return {
+    ...toRefs(state),
+    submitForm
+  };
+}
